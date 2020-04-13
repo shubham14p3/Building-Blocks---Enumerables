1ad96d90084d3c3e91db6a577445fd54de227aa4
shubham14p3 <shubham14p3@gmail.com>
2020-04-14 01:43:44 +0530

    Added Myeachwith index Test case

--- spec/rspec_enumerables.rb
+++ spec/rspec_enumerables.rb
@@ -26,5 +26,31 @@ RSpec.describe 'Enumerable' do
       end
       expect(sum).to eql(21)
     end
-  end 
+  end
+
+  describe '#my_each_with_index' do
+    it 'should return enum if no block given' do
+      expect(numeric_arr.my_each_with_index).to be_a(Enumerator)
+    end
+
+    it 'should return the array itself when a block is passed' do
+      expect(
+        numeric_arr.my_each_with_index do |el, i|
+        end
+      ).to eql(numeric_arr)
+    end
+
+    it 'should not return nil when called on an empty Enumerable' do
+      expect([].my_each_with_index).not_to eql(nil)
+    end
+
+    it 'should return the sum of all indexes of the array ' do
+      sum = 0
+      numeric_arr.my_each_with_index do |_el, i|
+        sum += i
+      end
+      expect(sum).to eql(15)
+    end
+  end
+
 end
